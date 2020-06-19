1775d1149b8dc4a3cd1f6d1c247a9bd39380f0f5
Chris Daigle <chris.daigle@uconn.edu>
2020-06-19 15:05:45 -0400

    feat: complete flask exercise 1

--- 02_lesson_SWE/BE_web/BackEndWorkspaceFiles/1_flask_exercise/worldbankapp/routes.py
+++ 02_lesson_SWE/BE_web/BackEndWorkspaceFiles/1_flask_exercise/worldbankapp/routes.py
@@ -6,10 +6,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
-    
-@app.route('/project-one')
+
+@app.route('/project_one')
 def project_one():
     return render_template('project_one.html')
+
+@app.route('/project_two')
+def project_two():
+    return render_template('project_two.html')
 
 # TODO: Add another route. You can use any names you want
 # Then go into the templates folder and add an html file that matches the file name you put in the render_template method. You can create a new file by going to the + sign at the top of the workspace and clicking on Create New File. Make sure to place the new html file in the templates folder.
